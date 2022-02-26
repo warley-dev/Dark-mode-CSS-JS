49902f1914efe2418c1b13323816232ba5fecc36
Warley Rodrigues <88275533+warleyrodrigues@users.noreply.github.com>
2022-02-25 21:08:32 -0300

    up

--- darkmode.js
+++ darkmode.js
@@ -1,19 +1,16 @@
 themeColorClear = "#ffffff"
 themeColorDark = "#191919"
 
-
 const nightMode = document.querySelector('#darkmod')
 const nightModeStorage = localStorage.getItem('idmode')
 
   nightMode.addEventListener('click', () => {
-      document.documentElement.classList.toggle('darkmode')
-      
+      document.documentElement.classList.toggle('darkmode')      
       if(document.documentElement.classList.contains('darkmode')) {
         localStorage.setItem('idmode', true)       
         document.querySelector("[name='theme-color']").content = themeColorClear;  
         return
       }
-
       localStorage.removeItem('idmode')   
       document.querySelector("[name='theme-color']").content = themeColorDark;   
   })    
